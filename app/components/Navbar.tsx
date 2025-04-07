544265aa10880a60ab24f5ac565d667685891bb1
Abhijeet Shashwat <asp616848@gmail.com>
2025-04-08 00:47:36 +0530

    Auth completed

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,19 +1,46 @@
 import React from 'react'
 import Link from 'next/link'
+import {auth, signIn, signOut} from '@/auth'
 
-const Navbar = () => {
+const Navbar = async() => {
+    const session = await auth();
     return (
         <header className='px-5 py-2 bg-white shadow-sm font-work-sans'>
             <nav className='flex justify-between items-center'>
                 <Link href="/">
                     <img src="/logo.png" alt="Logo" width={50} height={50} />
                 </Link>
+                <div className='flex items-center gap-5'>
+                    {session && session?.user ?(
+                        <>
+                        <Link href="/startup/create">
+                            <span>Create</span>
+                        </Link>
+                        <form action={async()=>{
+                            'use server';
+                            await signOut({redirectTo:"/"})}}>
+                            <button type="submit">LogOut</button>
+                        </form>
+                        <Link href={'/user/${session?.id}'}>
+                            <span>{session?.user?.name}</span>
+                        </Link>
+                        </>
+                    )
+                    :(
+                        <form action={async()=>{
+                            'use server';
+                            await signIn('github')
+                            }}>
+                            <button type="submit">Log In</button>
+                        </form>
+                    )
+                }
+                </div>
             </nav>
             <div className='flex items-center gap-5'>
-                
+
             </div>
         </header>
-        
     )
 }
 
