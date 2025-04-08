58f4592a716fdc437df636f9a3c5bf105771d5a7
Abhijeet Shashwat <asp616848@gmail.com>
2025-04-08 23:51:56 +0530

    working but ugly

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,47 +1,58 @@
 import type { Metadata } from "next";
 // import { Work_Sans } from "next/font/google";
 import localFont from "next/font/local";
-
 import "./globals.css";
 
-const workSans =localFont({
-  src:[
+const workSans = localFont({
+  src: [
     {
-      path: './fonts/WorkSans-Black.ttf',
+      path: "./fonts/WorkSans-Black.ttf",
       weight: "900",
-      style:"normal"
-    },{
-      path: './fonts/WorkSans-Bold.ttf',
-      weight: "700",
-      style:"normal"
-    },{
-      path: './fonts/WorkSans-ExtraBold.ttf',
+      style: "normal",
+    },
+    {
+      path: "./fonts/WorkSans-ExtraBold.ttf",
       weight: "800",
-      style:"normal"
-    },{
-      path: './fonts/WorkSans-SemiBold.ttf',
+      style: "normal",
+    },
+    {
+      path: "./fonts/WorkSans-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/WorkSans-SemiBold.ttf",
       weight: "600",
-      style:"normal"
-    },{
-      path: './fonts/WorkSans-Medium.ttf',
+      style: "normal",
+    },
+    {
+      path: "./fonts/WorkSans-Medium.ttf",
       weight: "500",
-      style:"normal"
-    },{
-      path: './fonts/WorkSans-Regular.ttf',
+      style: "normal",
+    },
+    {
+      path: "./fonts/WorkSans-Regular.ttf",
       weight: "400",
-      style:"normal"
-    },{
-      path: './fonts/WorkSans-Thin.ttf',
+      style: "normal",
+    },
+    {
+      path: "./fonts/WorkSans-Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "./fonts/WorkSans-Thin.ttf",
       weight: "200",
-      style:"normal"
-    },{
-      path: './fonts/WorkSans-ExtraLight.ttf',
+      style: "normal",
+    },
+    {
+      path: "./fonts/WorkSans-ExtraLight.ttf",
       weight: "100",
-      style:"normal"
+      style: "normal",
     },
   ],
-  variable:'--font-work-sans'
-})
+  variable: "--font-work-sans",
+});
 
 export const metadata: Metadata = {
   title: "Track-Startups",
@@ -53,13 +64,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  
   return (
     <html lang="en">
-      <body
-        className={workSans.variable}
-      >
+      <body className={`${workSans.variable} font-work-sans`}>
         {children}
       </body>
     </html>
+    
   );
 }
