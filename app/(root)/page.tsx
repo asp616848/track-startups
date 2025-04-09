f3c838e9bc737874768a51a4e208ca688d2c4587
Abhijeet Shashwat <asp616848@gmail.com>
2025-04-09 12:34:18 +0530

    uop

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import SearchForm from "../components/SearchForm";
 
 export default function Home() {
   return (
     <>
-    <h1 className="heading">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">Pitch Your Startups,<br/> connect with entrepreneurs</h1>
+        <p className="sub-heading">Submit Ideas, Vote on Pitches and create an impact</p>
+        <SearchForm/>
+      </section>
     </>
   );
 }
