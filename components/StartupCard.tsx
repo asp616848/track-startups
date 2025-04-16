0213f228dec412b71b4b4549bd3b106784c4b70d
Abhijeet Shashwat <asp616848@gmail.com>
2025-04-16 06:29:09 +0530

    fixes

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -37,10 +37,10 @@ const {
 return (
     <li className="startup-card group">
     <div className="flex-between">
-        <p className="startup_card_date">{formatDate(_createdAt)}</p>
-        <div className="flex gap-1.5">
-        <EyeIcon className="size-6 text-primary" />
-        <span className="text-16-medium">{views}</span>
+    <p className="startup_card_date">{formatDate(_createdAt)}</p>
+        <div className="icon-text-group">
+            <EyeIcon className="size-6 text-primary" />
+            <span className="text-16-medium">{views}</span>
         </div>
     </div>
 
