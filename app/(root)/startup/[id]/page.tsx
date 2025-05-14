ecffc76b3cd9510a4074e0fece0d495651a5b4f3
Abhijeet Shashwat <asp616848@gmail.com>
2025-05-14 20:45:31 +0530

    observed home page messed

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -2,6 +2,8 @@ import { sanityFetch } from '@/sanity/lib/live';
 import { DATA_QUERY } from "@/lib/queries";
 import React from 'react'
 import { formatDate } from '@/lib/utils';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export const experimental_ppr = true;
 
@@ -10,11 +12,34 @@ const page = async({params}: { params : Promise<{id:string}>}) =>{
     const {data: post} = await sanityFetch({query: DATA_QUERY, params:{id}})
     console.log(post?._createdAt)
     return (
+        <>
         <section className='pink_container !min-h-[230px]'>
             <p className="tag">{formatDate(post?._createdAt)}</p>
             <h1 className="heading">{post.title}</h1>
             <p className="sub-heading !max-1-5xl">{post.description}</p>
         </section>
+        <section className='section_container'>
+            <img
+                src={post.image}
+                alt="thumbnail"
+                className='w-full h-auto rounded-xl'
+            />
+            <div className='space-y-5 mt-10 max-w-4xl mx-auto'>
+                <div className='flex-between gap-5'>
+                    <Link href={`/user/${post.author?._id}`}
+                    className="flex gap-2 items-center mb-3"
+                    >
+                        <Image src = {post.author.image}
+                        alt="avatar"
+                        width={64}
+                        height={64}
+                        className="rounded-full drop-shadow-lg"
+                        />
+                    </Link>
+                </div>
+            </div>
+        </section>
+        </>
     );
 }
 
