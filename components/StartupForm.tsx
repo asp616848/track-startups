b8d443668ce8d471ff6c172011d2615f88852f63
Abhijeet Shashwat <asp616848@gmail.com>
2025-05-23 22:56:19 +0530

    Update StartupForm.tsx

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -2,9 +2,14 @@
 import React, { useState } from 'react'
 import { Input } from './ui/input';
 import { Textarea } from './ui/textarea';
+import MDEditor from "@uiw/react-md-editor"
+import { Button } from './ui/button';
+import { Send } from 'lucide-react';
 
 const StartupForm = () => {
+    const [pitch, setPitch] = useState(" ");
     const [errors, setErrors] = useState<Record<string,string>>({});
+    const isPending = false;
     return (
     <form action={()=>{}} className='startup-form'>
         <div>
@@ -63,6 +68,31 @@ const StartupForm = () => {
             {errors.link && <p className='startup-form_error'>
                 {errors.link}</p>}
         </div>
+
+        <div>
+            <label htmlFor='pitch' className='startup-form_label'>
+                Pitch
+            </label>
+            <MDEditor
+                value={pitch}
+                onChange={(value)=>{setPitch(value as string)}}
+                preview='edit'
+                style={{ borderRadius:20, overflow: "hidden"}}
+                textareaProps={
+                    {placeholder:"Briefly describe what problem gets solved"}
+                }
+                previewOptions={{
+                    disallowedElements:["style"]
+                }}
+            />
+            {errors.pitch && <p className='startup-form_error'>
+                {errors.pitch}</p>}
+        </div>
+        <Button
+            type="submit" className='startup-form_btn' disabled={isPending}>
+                {isPending? "Submitting..." : "Submit Your Pitch"}
+                <Send className='size-6 ml-2'/>
+            </Button>
     </form>
     )
 }
